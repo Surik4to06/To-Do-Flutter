0db199712599095f6cbbffd0289ccac47462785d
Surik4to06 <richardasso56@gmail.com>
2025-10-03 20:16:21 -0300

    Create build.gradle.kts

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.tarefas_app"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.tarefas_app"
+        minSdk = flutter.minSdkVersion
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    // Alternativa moderna usando toolchain (Gradle >= 8)
+    // kotlin {
+    //     jvmToolchain(11)
+    // }
+}
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-firestore")
+}
